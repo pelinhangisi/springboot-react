fea3979f5bee6e89222e0fa5f222913aeccb0bd8
Pelin Hangişi <pelinhangisi@Pelins-MBP.home>
2025-04-05 16:27:43 +0300

    Error messages and Control and check the username scenario done

--- ws/src/main/java/com/hangisip/ws/user/UserController.java
+++ ws/src/main/java/com/hangisip/ws/user/UserController.java
@@ -2,9 +2,14 @@ package com.hangisip.ws.user;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hangisip.ws.error.ApiError;
 import com.hangisip.ws.shared.GenericMessage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -15,9 +20,20 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/api/v1/users")
-    GenericMessage createUser(@RequestBody User user) {
+    ResponseEntity<?> createUser(@RequestBody User user) {
+        if (user.getUsername() == null || user.getUsername().isEmpty()) {
+            ApiError apiError = new ApiError();
+            apiError.setPath("/api/v1/users");
+            apiError.setMessage("Validation error");
+            apiError.setStatus(400);
+            Map<String, String> validationErrors = new HashMap<>();
+            validationErrors.put("username", "Zorunlu Alan");
+            apiError.setValidationErrors(validationErrors);
+            return ResponseEntity.badRequest().body(apiError);
+        }
+
         userService.save(user);
-        return new GenericMessage("Kullanıcı Oluşturuldu");
+        return ResponseEntity.ok(new GenericMessage("Kullanıcı Oluşturuldu"));
     }
 
 }
