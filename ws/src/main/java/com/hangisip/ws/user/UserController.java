8552bf74dd521393cdcb75928b434edfba1d3063
Pelin Hangişi <pelinhangisi@Pelins-MBP.home>
2025-06-01 00:52:33 +0300

    using the ExceptionHandler anotation and create ApiError

--- ws/src/main/java/com/hangisip/ws/user/UserController.java
+++ ws/src/main/java/com/hangisip/ws/user/UserController.java
@@ -5,11 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hangisip.ws.error.ApiError;
 import com.hangisip.ws.shared.GenericMessage;
 
-import java.util.HashMap;
-import java.util.Map;
+import jakarta.validation.Valid;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -20,20 +23,27 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/api/v1/users")
-    ResponseEntity<?> createUser(@RequestBody User user) {
-        if (user.getUsername() == null || user.getUsername().isEmpty()) {
-            ApiError apiError = new ApiError();
-            apiError.setPath("/api/v1/users");
-            apiError.setMessage("Validation error");
-            apiError.setStatus(400);
-            Map<String, String> validationErrors = new HashMap<>();
-            validationErrors.put("username", "Zorunlu Alan");
-            apiError.setValidationErrors(validationErrors);
-            return ResponseEntity.badRequest().body(apiError);
-        }
-
+    GenericMessage createUser(@Valid @RequestBody User user) {
         userService.save(user);
-        return ResponseEntity.ok(new GenericMessage("Kullanıcı Oluşturuldu"));
+        return new GenericMessage("Kullanıcı Oluşturuldu");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<ApiError> handleMethodArgNotValidEx(MethodArgumentNotValidException exception) {
+        ApiError apiError = new ApiError();
+        apiError.setPath("/api/v1/users");
+        apiError.setMessage("Validation error");
+        apiError.setStatus(400);
+        /*
+         * Map<String, String> validationErrors = new HashMap<>();
+         * for (var fieldError : exception.getBindingResult().getFieldErrors()) {
+         * validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+         * }
+         */
+        var validationErrors = exception.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+        apiError.setValidationErrors((validationErrors));
+        return ResponseEntity.badRequest().body(apiError);
     }
 
 }
